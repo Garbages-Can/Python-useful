b0a4f2ef6b7781801908e8e40ba7e0bf3c508bd5
ltoddy <1205446803@qq.com>
2017-10-27 23:53:25 +0800

    markdown to html

---
+++ markdowntohtml/markdowntohtml.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import argparse
+import markdown
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--filename', help='你的markdown文件名(.md为后缀).')
+parser.add_argument('-o', '--output', default='output.html',
+                    help='输出的html文件名(.html为后缀),默认名为output.html.')
+args = parser.parse_args()
+
+# check arguments:
+if not (str(args.filename).endswith('.md') and str(args.output).endswith('.html')):
+    print(parser.print_help())
+    sys.exit(-1)
+
+with open(args.filename, 'r') as fp:
+    text = fp.read()
+    print('输入源文件内容:', text, sep='\n')
+    html = markdown.markdown(text)
+    output_file = args.output
+    with open(output_file, 'w') as output:
+        print('输出相应的html为:', html, sep='\n')
+        output.write(html)

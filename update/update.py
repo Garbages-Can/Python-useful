07abede5245de6667d830a89af26f4a442ea4f28
ltoddy <taoliu14@acm.org>
2018-05-13 22:44:20 +0800

    update pip API

--- update/update.py
+++ update/update.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import pip
+from pip._internal import get_installed_distributions
 from subprocess import call
 
-for dist in pip.get_installed_distributions():
+for dist in get_installed_distributions():
     call('pip3 install --upgrade ' + dist.project_name, shell=True)
